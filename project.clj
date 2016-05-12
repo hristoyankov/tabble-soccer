ee1adef3e7c0de36640c9e0cb4485278c671e72b
Hristo Yankov <ico.yankov@gmail.com>
2016-05-12 08:37:06 +0300

    Add ring library.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject tabble-soccer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tabble soccer portal"
+  :url "TBA"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]]
+
+  :main tabble-soccer.core)
--- src/tabble_soccer/core.clj
+++ src/tabble_soccer/core.clj
@@ -1,6 +1,6 @@
 (ns tabble-soccer.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main []
+  (println "Hello world"))
+              
+
