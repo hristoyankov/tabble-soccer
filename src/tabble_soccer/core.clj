fd231433ddcd341a8fc5b46bf7a7f52bda516b11
Hristo Yankov <ico.yankov@gmail.com>
2016-05-12 09:21:47 +0300

    Add basic hello world handler with wrap-reload.

--- src/tabble_soccer/core.clj
+++ src/tabble_soccer/core.clj
@@ -1,6 +1,19 @@
-(ns tabble-soccer.core)
+(ns tabble-soccer.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
-(defn -main []
-  (println "Hello world"))
+(defn handler [req]
+  (if (= "/hello" (:uri req))
+    {:status 200
+     :body "<h1> Hello world </h1>"
+     :headers {}}
+    {:status 404
+     :body "<h1> Page not found </h1>"
+     :headers {}}))
+
+(def app handler)
+
+(defn -main [port]
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
               
 
