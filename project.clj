2df4414c21f2174599ee7ebc7724cc534406fc9b
Hristo Yankov <ico.yankov@gmail.com>
2016-05-13 08:08:37 +0300

    Add travis CI support.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.4.0"]
-                 [ring/ring-mock "0.3.0"]]
+                 [ring "1.4.0"]]
 
-  :main table-soccer.core)
+  :main table-soccer.core
+
+  :profiles {:test {:dependencies [[ring/ring-mock "0.3.0"]]}})
