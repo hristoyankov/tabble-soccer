d5c7be730c5a655452555fe1872ca37fa584932d
Hristo Yankov <ico.yankov@gmail.com>
2016-05-17 08:46:31 +0300

    Update min-lein-version according to Travis CI.

--- project.clj
+++ project.clj
@@ -9,6 +9,9 @@
                  [compojure "1.5.0"]
                  [hiccup "1.0.5"]]
 
+  ;; Travis CI assumes 2.4.x leiningen version
+  :min-lein-version "2.4.0"
+
   :main table-soccer.core
 
   :profiles {:test {:dependencies [[ring/ring-mock "0.3.0"]]}})
