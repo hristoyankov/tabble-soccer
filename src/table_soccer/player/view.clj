f3f65f81bdb44213ae6afbfaca7fbafb0edbef23
Hristo Yankov <ico.yankov@gmail.com>
2016-05-17 08:41:56 +0300

    Add initial players table (static info).

--- src/table_soccer/player/view.clj
+++ src/table_soccer/player/view.clj
@@ -8,6 +8,17 @@
           [:title "Players page"]]
          [:body
           [:div
-           [:h1 "Hello players"]]]))
+           [:h1 "List of players"]
+           (if (seq players)
+             [:table
+              [:thead
+               [:tr
+                [:th "ID"]
+                [:th "Name"]]]
+              [:tbody
+               (for [p players]
+                 [:tr
+                  [:td (:id p)]
+                  [:td (:name p)]])]])]]))
           
            
